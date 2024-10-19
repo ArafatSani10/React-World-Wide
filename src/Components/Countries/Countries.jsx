6eb9734b382a9e1636e2b0fd230d0cbb0e8dc7cc
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2024-10-19 21:40:42 +0600

    load by data and added to flags and country

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Country from "../Country/Country";
 
 
 const Countries = () => {
@@ -12,7 +13,10 @@ const Countries = () => {
     },[])
     return (
         <div>
-            <h3>Countries</h3>
+            <h3>Countries : {countries.length}</h3>
+            {
+              countries.map(country => <Country key={country.cca3} country={country}></Country> )  
+            }
         </div>
     );
 };
